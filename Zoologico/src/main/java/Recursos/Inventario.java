ebac3cac17f8706f3b2d91980a71072ef546e5ca
fferrlop <fferrlop@myuax.com>
2024-03-13 20:28:53 +0100

    recursos

--- Zoologico/src/main/java/Recursos/Inventario.java
+++ Zoologico/src/main/java/Recursos/Inventario.java
@@ -1,4 +1,29 @@
 package Recursos;
 
-public class Inventario {
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+class Inventario {
+    private Map<String, Recurso> inventario;
+
+    public Inventario() {
+        this.inventario = new HashMap<>();
+    }
+
+    public void agregarRecurso(Recurso recurso) {
+        inventario.put(recurso.getNombre(), recurso);
+    }
+
+    public Recurso obtenerRecurso(String nombre) {
+        return inventario.get(nombre);
+    }
+
+    public void mostrarInventario() {
+        System.out.println("Inventario:");
+        for (Recurso recurso : inventario.values()) {
+            System.out.println(recurso.getNombre() + " - Cantidad: " + recurso.getCantidad());
+        }
+    }
 }
